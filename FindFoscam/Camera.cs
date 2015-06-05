9e955e2f82f2c49260bd4cbfea1947c7843bdc38
Gus Catalano <gus@guscatalano.com>
2015-06-05 09:18:40 -0700

    initial

---
+++ FindFoscam/Camera.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindFoscam
+{
+    class Camera
+    {
+
+        public string ID {
+            get;
+            internal set;
+        }
+
+        public string Name {
+            get;
+            internal set;
+        }
+
+        public IPEndPoint SourceEP
+        {
+            get;
+            internal set;
+        }
+
+        public IPAddress IP {
+            get;
+            internal set;
+        }
+
+        public IPAddress Mask {
+            get;
+            internal set;
+        }
+
+        public IPAddress Gateway {
+            get;
+            internal set;
+        }
+
+        public IPAddress DNS {
+            get;
+            internal set;
+        }
+
+        public string SysVersion {
+            get;
+            internal set;
+        }
+
+        public string AppVersion {
+            get;
+            internal set;
+        }
+
+        public int Port {
+            get;
+            internal set;
+        }
+
+        public bool DHCPEnabled {
+            get;
+            internal set;
+        }
+
+        public static bool IsCameraPacket(Byte[] receiveBytes)
+        {
+            return !(receiveBytes.Count() < 65);
+        }
+        public Camera(Byte[] receiveBytes, IPEndPoint source)
+        {
+            
+            
+
+            SourceEP = source;
+            Byte[] camID = new Byte[13];
+            int startIndex = 23;
+            int currentIndex = startIndex;
+            Array.Copy(receiveBytes, currentIndex, camID, 0, 13);
+            ID = Encoding.Default.GetString(camID);
+
+            currentIndex += 13;
+            Byte[] camname = new Byte[21];
+            Array.Copy(receiveBytes, currentIndex, camname, 0, 21);
+            Name = Encoding.Default.GetString(camname);
+
+            currentIndex += 21;
+            Byte[] ipb = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, ipb, 0, 4);
+            int ip1 = BitConverter.ToInt32(ipb, 0);
+            IP = new IPAddress(ip1);
+
+            currentIndex += 4;
+            Byte[] maskB = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, maskB, 0, 4);
+            int mask1 = BitConverter.ToInt32(maskB, 0);
+            Mask = new IPAddress(mask1);
+
+            currentIndex += 4;
+            Byte[] gw = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, gw, 0, 4);
+            int gw1 = BitConverter.ToInt32(gw, 0);
+            Gateway = new IPAddress(gw1);
+
+            currentIndex += 4;
+            Byte[] dnsB = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, dnsB, 0, 4);
+            Array.Reverse(dnsB);
+            int dns1 = BitConverter.ToInt32(dnsB, 0);
+            
+            DNS = new IPAddress(dns1);
+
+
+            currentIndex += 4;
+            Byte[] reserve = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, reserve, 0, 4);
+
+            currentIndex += 4;
+            Byte[] sys = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, sys, 0, 4);
+            SysVersion = sys[0] + "." + sys[1] + "." + sys[2] + "." + sys[3];
+
+            currentIndex += 4;
+            Byte[] app = new Byte[4];
+            Array.Copy(receiveBytes, currentIndex, app, 0, 4);
+            AppVersion = app[0] + "." + app[1] + "." + app[2] + "." + app[3];
+
+            currentIndex += 4;
+            Byte[] portB = new Byte[2];
+            Array.Copy(receiveBytes, currentIndex, portB, 0, 2);
+            Array.Reverse(portB);
+            Port = BitConverter.ToInt16(portB, 0);
+
+            currentIndex += 2;
+            Byte[] dhcp = new Byte[1];
+            Array.Copy(receiveBytes, currentIndex, dhcp, 0, 1);
+            DHCPEnabled = BitConverter.ToBoolean(dhcp, 0);
+
+            
+        
+        }
+
+        public void PrintInfo()
+        {
+            Console.WriteLine("==================================");
+            Console.WriteLine("Cam ID: " + ID);
+            Console.WriteLine("Cam Name: " + Name);
+            Console.WriteLine("IP: " + IP);
+            Console.WriteLine("Mask: " + Mask);
+            Console.WriteLine("GW: " + Gateway);
+            Console.WriteLine("DNS: " + DNS);
+            Console.WriteLine("Sys: " + SysVersion);
+            Console.WriteLine("App: " + AppVersion);
+            Console.WriteLine("Port: " + Port);
+            Console.WriteLine("DHCP: " + DHCPEnabled);
+            Console.WriteLine("==================================");
+        }
+    }
+}
---
+++ FindFoscam/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FindFoscam
+{
+    class Program
+    {
+
+        public static List<Camera> cameras = new List<Camera>();
+        public static UdpClient udpclient = new UdpClient(10000);
+        public static void DiscoverThread()
+        {
+            //UdpClient udpclient = new UdpClient();
+            while (true)
+            {
+                try
+                {
+                    //This is the packet that discovers camerasw
+                    Byte[] buffer = null;//11
+                    //4d:4f:5f:49:00:00:00:00:00:00:00:00:00:00:00:04:00:00:00:04:00:00:00:00:00:00:01
+                    buffer = new byte[] { 
+                0x4d, 0x4f, 0x5f, 0x49, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 
+                0x00, 0x04, 0x00, 0x00, 
+                0x00, 0x04, 0x00, 0x00, 
+                0x00, 0x00, 0x00, 0x00, 0x01 };
+
+                    //Not sure what this packet is for, but it's sent... It's not needed.
+                    Byte[] buffer2nd = new byte[] { 
+                0xb4, 0x9a, 0x70, 0x4d, 0x00};
+
+
+
+                    IPAddress multicastaddress = IPAddress.Parse("255.255.255.255");
+                    IPEndPoint remoteep = new IPEndPoint(multicastaddress, 10000);
+                    udpclient.EnableBroadcast = true;
+                    udpclient.Send(buffer, buffer.Length, remoteep);
+                    //udpclient.Send(buffer2nd, buffer2nd.Length, remoteep);
+
+                    Thread.Sleep(10000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Discover: " + e.ToString());
+                }
+            }
+        }
+
+        public static void RecDiscover()
+        {
+            IPEndPoint recPoint = new IPEndPoint(IPAddress.Broadcast, 1);
+            
+            while (true)
+            {
+                try
+                {
+                    Byte[] receiveBytes = udpclient.Receive(ref recPoint);
+                    if (Camera.IsCameraPacket(receiveBytes))
+                    {
+                        Camera cam = new Camera(receiveBytes, recPoint);
+                        cam.PrintInfo();
+                        cameras.Add(cam);
+                    }
+                    Thread.Sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Rec: " + e.ToString());
+                }
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Press ENTER to start sending messages");
+            Console.ReadLine();
+            Thread t = new Thread(new ThreadStart(DiscoverThread));
+            t.Start();
+
+            Thread t2 = new Thread(new ThreadStart(RecDiscover));
+            t2.Start();
+            while (true)
+            {
+               // Console.WriteLine("")
+                foreach (Camera cam in cameras)
+                {
+                    cam.PrintInfo();
+                }
+                Thread.Sleep(5000);
+            }
+            
+            
+            
+            
+            Console.WriteLine("All Done! Press ENTER to quit.");
+            Console.ReadLine(); 
+        }
+    }
+}
